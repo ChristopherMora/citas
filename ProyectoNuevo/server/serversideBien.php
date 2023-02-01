ecb5e2f90b01901456d974e70c5a25f2d0312725
ChristopherMora <95389955+ChristopherMora@users.noreply.github.com>
2023-01-31 22:53:24 -0600

    borrado

--- ProyectoNuevo/server/serversideBien.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'serverside.php';
-$table_data->get('registros','id_folio',array('id_folio', 'nombre_completo','contacto','Asunto','Telefono','Atendio'));
-
-?>
--- ProyectoNuevo/server/serversideConexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("HOST_SS","localhost");
-define("DATABASE_SS","regis-report");
-define("USER_SS","xcuser");
-define("PASSWORD_SS","root");
-?>
