e52c613de1588c7414b7a92c11f01748fcebbf9a
ChristopherMora <95389955+ChristopherMora@users.noreply.github.com>
2023-02-01 03:16:29 -0600

    as

--- ProyectoNuevo/frontendcitas/index.php
+++ ProyectoNuevo/frontendcitas/index.php
@@ -106,10 +106,11 @@ if (isset($_POST['correo']) && isset($_POST['password'])) {
             </div>
         </div>
     </div>
+    <script src="assets/js/localEstorage.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="assets/js/script.min.js"></script>
-    <script src="assets/js/localEstorage.js"></script>
+    
 </body>
 
 </html>
