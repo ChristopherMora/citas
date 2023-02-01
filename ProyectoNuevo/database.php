8446ff67743a7255a010ef3672a4a8aca691bb18
IbrainMartinez <reivax127@gmail.com>
2023-01-31 23:38:06 -0600

    database

--- ProyectoNuevo/database.php
+++ ProyectoNuevo/database.php
@@ -11,8 +11,8 @@ class Database{
     public function __construct(){
         $this->host = 'localhost';
         $this->db = 'regis-report';
-        $this->user = 'loco';
-        $this->password = 'loco';
+        $this->user = 'root';
+        $this->password = '';
         $this->charset = 'utf8mb4';
     }
 
